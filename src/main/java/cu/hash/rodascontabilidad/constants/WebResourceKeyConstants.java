d0a7746e28dc0fbcd9102e008ca776639e23968a
henry1806 <henry1806@nauta.cu>
2021-09-26 16:10:45 -0400

    Web Rsc Key Constants

--- src/main/java/cu/hash/rodascontabilidad/constants/WebResourceKeyConstants.java
+++ src/main/java/cu/hash/rodascontabilidad/constants/WebResourceKeyConstants.java
@@ -1,4 +1,12 @@
 package cu.hash.rodascontabilidad.constants;
 
 public class WebResourceKeyConstants {
+    public interface Endpoints{
+        String GET_ALL = "/all";
+        String GET_BY_ID = "/id";
+        String UPDATE = "/update";
+        String SAVE = "/save";
+        String DELETE = "/delete";
+        String DELETE_BY_ID = "/delete/id";
+    }
 }
