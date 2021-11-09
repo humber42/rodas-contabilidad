90403d3d55977d50fe6a543a5924c60b6721c4c1
henry1806 <henry1806@nauta.cu>
2021-11-09 15:24:10 -0500

    V4,V5 FLYWAY BD

---
+++ src/main/resources/db/migration/V4__sequences.sql
@@ -0,0 +1,385 @@
+
+-- ----------------------------
+-- Sequence structure for actividad_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."actividad_id_seq";
+CREATE SEQUENCE "public"."actividad_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for cargo_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."cargo_id_seq";
+CREATE SEQUENCE "public"."cargo_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for categoria_agrupacion_elemento_gasto_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."categoria_agrupacion_elemento_gasto_id_seq";
+CREATE SEQUENCE "public"."categoria_agrupacion_elemento_gasto_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for categoria_agrupacion_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."categoria_agrupacion_id_seq";
+CREATE SEQUENCE "public"."categoria_agrupacion_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for categoria_agrupacion_indirecta_elemento_gasto_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."categoria_agrupacion_indirecta_elemento_gasto_id_seq";
+CREATE SEQUENCE "public"."categoria_agrupacion_indirecta_elemento_gasto_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for categoria_agrupacion_indirecta_subelemento_gasto_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."categoria_agrupacion_indirecta_subelemento_gasto_id_seq";
+CREATE SEQUENCE "public"."categoria_agrupacion_indirecta_subelemento_gasto_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for cliente_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."cliente_id_seq";
+CREATE SEQUENCE "public"."cliente_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for coeficiente_gastos_bancarios_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."coeficiente_gastos_bancarios_id_seq";
+CREATE SEQUENCE "public"."coeficiente_gastos_bancarios_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for coeficiente_gastos_distribucion_ventas_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."coeficiente_gastos_distribucion_ventas_id_seq";
+CREATE SEQUENCE "public"."coeficiente_gastos_distribucion_ventas_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for coeficiente_gastos_generales_admon_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."coeficiente_gastos_generales_admon_id_seq";
+CREATE SEQUENCE "public"."coeficiente_gastos_generales_admon_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for coeficiente_gastos_indirectos_produccion_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."coeficiente_gastos_indirectos_produccion_id_seq";
+CREATE SEQUENCE "public"."coeficiente_gastos_indirectos_produccion_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for coeficiente_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."coeficiente_id_seq";
+CREATE SEQUENCE "public"."coeficiente_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for elemento_gasto_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."elemento_gasto_id_seq";
+CREATE SEQUENCE "public"."elemento_gasto_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for etapa_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."etapa_id_seq";
+CREATE SEQUENCE "public"."etapa_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for ficha_costo_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."ficha_costo_id_seq";
+CREATE SEQUENCE "public"."ficha_costo_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for gasto_directo_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."gasto_directo_id_seq";
+CREATE SEQUENCE "public"."gasto_directo_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for gasto_indirecto_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."gasto_indirecto_id_seq";
+CREATE SEQUENCE "public"."gasto_indirecto_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for gastos_actividad_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."gastos_actividad_id_seq";
+CREATE SEQUENCE "public"."gastos_actividad_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for material_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."material_id_seq";
+CREATE SEQUENCE "public"."material_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for norma_consumo_ficha_costo_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."norma_consumo_ficha_costo_id_seq";
+CREATE SEQUENCE "public"."norma_consumo_ficha_costo_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for normas_consumo_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."normas_consumo_id_seq";
+CREATE SEQUENCE "public"."normas_consumo_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for orden_facturacion_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."orden_facturacion_id_seq";
+CREATE SEQUENCE "public"."orden_facturacion_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for orden_trabajo_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."orden_trabajo_id_seq";
+CREATE SEQUENCE "public"."orden_trabajo_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for personas_autorizadas_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."personas_autorizadas_id_seq";
+CREATE SEQUENCE "public"."personas_autorizadas_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for plan_produccion_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."plan_produccion_id_seq";
+CREATE SEQUENCE "public"."plan_produccion_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for reporte_diario_laboral_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."reporte_diario_laboral_id_seq";
+CREATE SEQUENCE "public"."reporte_diario_laboral_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for subelemento_gasto_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."subelemento_gasto_id_seq";
+CREATE SEQUENCE "public"."subelemento_gasto_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for tipo_actividad_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."tipo_actividad_id_seq";
+CREATE SEQUENCE "public"."tipo_actividad_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for tipo_gasto_directo_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."tipo_gasto_directo_id_seq";
+CREATE SEQUENCE "public"."tipo_gasto_directo_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for tipo_gasto_indirecto_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."tipo_gasto_indirecto_id_seq";
+CREATE SEQUENCE "public"."tipo_gasto_indirecto_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for trabajador_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."trabajador_id_seq";
+CREATE SEQUENCE "public"."trabajador_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for ueb_actividades_plan_produccion_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."ueb_actividades_plan_produccion_id_seq";
+CREATE SEQUENCE "public"."ueb_actividades_plan_produccion_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for ueb_etapa_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."ueb_etapa_id_seq";
+CREATE SEQUENCE "public"."ueb_etapa_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for ueb_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."ueb_id_seq";
+CREATE SEQUENCE "public"."ueb_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for unidad_medida_id_seq
+-- ----------------------------
+DROP SEQUENCE "public"."unidad_medida_id_seq";
+CREATE SEQUENCE "public"."unidad_medida_id_seq"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
